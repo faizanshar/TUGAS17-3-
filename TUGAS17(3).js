09d3d07a6eee8a1d16edfa6f95ddfc2de8539b71
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-10 22:28:19 +0700

    first commit

---
+++ TUGAS17(3).js
@@ -0,0 +1,5 @@
+var santriPondok = ['wandi','aldi','acep','putra','hafif','syofyan','rahmad']
+
+for(i = 1; i < santriPondok.length; i++){
+    document.write(i + '. ' + santriPondok[i] + "<br>")
+}
